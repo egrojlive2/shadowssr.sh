7995e27f8c124d477562d6062ef5a183528e1e81
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-08-16 08:08:01 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,39 @@
+.class public Lcom/example/prueba2/MainActivity;
+.super Landroidx/appcompat/app/AppCompatActivity;
+.source "MainActivity.java"
 
+
+# instance fields
+.field vid:Landroid/widget/VideoView;
+
+
+# direct methods
+.method public constructor <init>()V
+    .registers 1
+
+    .prologue
+    .line 10
+    invoke-direct {p0}, Landroidx/appcompat/app/AppCompatActivity;-><init>()V
+
+    return-void
+.end method
+
+
+# virtual methods
+.method protected onCreate(Landroid/os/Bundle;)V
+    .registers 4
+    .param p1, "savedInstanceState"    # Landroid/os/Bundle;
+
+    .prologue
+    .line 14
+    invoke-super {p0, p1}, Landroidx/appcompat/app/AppCompatActivity;->onCreate(Landroid/os/Bundle;)V
+
+    .line 15
+    const v0, 0x7f0a001c
+
+    invoke-virtual {p0, v0}, Lcom/example/prueba2/MainActivity;->setContentView(I)V
+
+    .line 20
     const v0, 0x7f0700b3
 
     invoke-virtual {p0, v0}, Lcom/example/prueba2/MainActivity;->findViewById(I)Landroid/view/View;
@@ -9,9 +44,7 @@
 
     iput-object v0, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
 
-
-
-
+    .line 21
     iget-object v0, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
 
     new-instance v1, Lcom/example/prueba2/MainActivity$1;
@@ -19,3 +52,57 @@
     invoke-direct {v1, p0}, Lcom/example/prueba2/MainActivity$1;-><init>(Lcom/example/prueba2/MainActivity;)V
 
     invoke-virtual {v0, v1}, Landroid/widget/VideoView;->setOnCompletionListener(Landroid/media/MediaPlayer$OnCompletionListener;)V
+
+    .line 27
+    iget-object v0, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
+
+    invoke-virtual {p0, v0}, Lcom/example/prueba2/MainActivity;->playVideo(Landroid/view/View;)V
+
+    .line 28
+    return-void
+.end method
+
+.method protected onResume()V
+    .registers 2
+
+    .prologue
+    .line 32
+    iget-object v0, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
+
+    invoke-virtual {p0, v0}, Lcom/example/prueba2/MainActivity;->playVideo(Landroid/view/View;)V
+
+    .line 33
+    invoke-super {p0}, Landroidx/appcompat/app/AppCompatActivity;->onResume()V
+
+    .line 34
+    return-void
+.end method
+
+.method public playVideo(Landroid/view/View;)V
+    .registers 5
+    .param p1, "v"    # Landroid/view/View;
+
+    .prologue
+    .line 41
+    const-string v0, "android.resource://com.soyrojo/2131099745"
+
+    .line 42
+    .local v0, "path":Ljava/lang/String;
+    invoke-static {v0}, Landroid/net/Uri;->parse(Ljava/lang/String;)Landroid/net/Uri;
+
+    move-result-object v1
+
+    .line 44
+    .local v1, "u":Landroid/net/Uri;
+    iget-object v2, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
+
+    invoke-virtual {v2, v1}, Landroid/widget/VideoView;->setVideoURI(Landroid/net/Uri;)V
+
+    .line 46
+    iget-object v2, p0, Lcom/example/prueba2/MainActivity;->vid:Landroid/widget/VideoView;
+
+    invoke-virtual {v2}, Landroid/widget/VideoView;->start()V
+
+    .line 48
+    return-void
+.end method
